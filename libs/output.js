4243486d96c465da9a96ebeac36536b556956aa9
franck34 <eviltik@gmail.com>
2013-06-03 10:24:10 +0200

    start output formater

---
+++ libs/output.js
@@ -0,0 +1,24 @@
+var argv = require('optimist').argv;
+
+var outputJson = function(o) {
+    console.log(JSON.stringify(o));
+}
+
+var outputRaw = function(o) {
+    if (o.error) {
+        return console.log('Error: ',o.error)
+    }
+    var str = '';
+    for (var key in o) str+=o[key]+'|';
+    str = str.replace(/\| ?$/,'');
+    console.log(str);
+}
+
+var output = function(o) {
+    if (argv.output == 'json' || argv.json) {
+        return outputJson(o);
+    }
+    return outputRaw(o);
+}
+
+module.exports = output;
