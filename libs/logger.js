9987a3980d5a0e1f05b4bf8b8d20d79a83a82757
eviltik <eviltik@gmail.com>
2018-02-10 14:35:23 +0100

    clean up

--- libs/logger.js
+++ libs/logger.js
@@ -1,47 +1,38 @@
-var winston = require('winston');
+const winston = require('winston');
+
 winston.cli();
 
-
-var logger = new (winston.Logger)({
+const logger = new (winston.Logger)({
 	transports: [
 		new (winston.transports.Console)({
 			colorize:true,
 			timestamp:function() {
-				var d = new Date();
-				var h = (d.getHours()<10 ? "0" : "")+d.getHours();
-				var m = (d.getMinutes()<10 ? "0" : "")+d.getMinutes();
-				var s = (d.getSeconds()<10 ? "0" : "")+d.getSeconds();
-				var mm = d.getMilliseconds();
+				let d = new Date();
+				let h = (d.getHours()<10 ? "0" : "")+d.getHours();
+				let m = (d.getMinutes()<10 ? "0" : "")+d.getMinutes();
+				let s = (d.getSeconds()<10 ? "0" : "")+d.getSeconds();
+				let mm = d.getMilliseconds();
 				if (mm<10) mm='0'+mm;
 				if (mm<100) mm='0'+mm;
-				var str = h+':'+m+':'+s+'.'+mm;
+				let str = h+':'+m+':'+s+'.'+mm;
 				return str;
 			}
 		})
 	]
 });
 
+let i = 0;
+let debug = false;
 
-var i = 0;
-var debug = false;
-
-process.argv.forEach(function(arg) {
+process.argv.forEach(arg => {
     if (arg=='-d') debug = true;
     i++;
 });
 
 if (!debug) {
-	logger.debug = function() {};
+	logger.debug = () => {};
 }
-
-var fatalError = function(msg) {
-    logger.error(msg.replace(/\n$/,''));
-    logger.error('Aborting');
-    process.exit();
-}
-
 
 module.exports = {
-    log:logger,
-    fatalError:fatalError
+    log:logger
 }
